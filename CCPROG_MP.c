274c042dd2b879e27dfc933e988dc3886fcb2e10
nightmarixowo <paulnathanmangahas@gmail.com>
2026-03-02 14:53:56 +0800

    user interface

--- CCPROG_MP.c
+++ CCPROG_MP.c
@@ -2,18 +2,49 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct {
-	int i;
-	float j;
-}letters;
+/* FUNCTION DECLRATIONS */
 
 
 int main()
 {
 
-    // change
-    printf("thank you amanda\n");
-    printf("ccprrog mp project let's go\n");
+	int loginSelect;
+	int loginStatus = 1;
+	
+	//LOGIN UI SCREEN
+	printf("+---------------------------------+\n");
+	printf("| SOLAR ENERGY INFORMATION SYSTEM |\n");
+	printf("+---------------------------------+\n");
+	printf("|                                 |\n");
+	printf("| 1. LOGIN                        |\n");
+	printf("| 2. REGISTER                     |\n");
+	printf("| 3. ACCOUNT RECOVERY             |\n");
+	printf("|                                 |\n");
+	printf("| 0. EXIT PROGRAM                 |\n");		
+	printf("|                                 |\n");	
+	printf("+---------------------------------+\n");	
+	
+	while(loginStatus)
+	{
 
-return 0;
+		printf("-> ");
+		scanf("%d", &loginSelect);
+
+		switch(loginSelect)
+			{
+			case 1: printf("LOGIN FUNCTION NOT YET INPUTED\n"); //LOGIN FUNCTION - NOT CREATED YET
+			break;
+			case 2: printf("REGISTER FUNCTION NOT YET INPUTED\n"); //REGISTER FUNCTION - NOT CREATED YET
+			break;
+			case 3: printf("PASSWORD RECOVERY FUNCTION NOT YET INPUTED\n"); //PW RECOVERY FUNCTION - NOT CREATED YET
+			break;
+			case 0: loginStatus = 0; //EXIT PROGRAM
+			default: printf("INVALID SELECTION! PLEASE TRY AGAIN!\n"); //incase of wrong input
+			break;
+			}
+		
+	}
+	
+
+	return 0;
 }
